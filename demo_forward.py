c285effe8b7619c71ebaf37bb620ca4f4c9082dd
Philipp Gayret <philipp@release-engineers.com>
2024-05-23 21:30:43 +0200

    refactor: update item usage in MainApp

--- demo_forward.py
+++ demo_forward.py
@@ -21,7 +21,8 @@ class Item:
 class MainApp(App):
     def __init__(self):
         super().__init__()
-        self.forward_select = ForwardSelect(Item(ItemFieldEnum.BOB), "my_field", ForwardSelect.enum_options(ItemFieldEnum))
+        self.item = Item(ItemFieldEnum.BOB)
+        self.forward_select = ForwardSelect(self.item, "my_field", ForwardSelect.enum_options(ItemFieldEnum))
 
     def compose(self) -> ComposeResult:
         yield self.forward_select
@@ -30,7 +31,7 @@ class MainApp(App):
     @on(Select.Changed)
     def refresh_label(self):
         label = self.query_one(Label)
-        label.update(f"Selected: {self.forward_select.item.my_field}")
+        label.update(f"Selected: {self.item.my_field}")
 
 
 if __name__ == "__main__":
